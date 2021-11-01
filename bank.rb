320bd4cd36e4f54f1071310b06ca6f32f595d595
LAPTOP-56NU4RUS\jenni <jennifer7793@gmail.com>
2021-11-01 17:31:53 +0800

    rspec

--- bank.rb
+++ bank.rb
@@ -1,13 +1,32 @@
+# class = BankError < StandardError
+# end
+
+# class Bank
+#   def initialize(money)
+#     @money = money
+#   end
+
+#   def deposit(amount)
+#     if amount <= 0
+#       raise BankError.new("amount must greater than zero")
+#     end
+
+#     def balance
+#       @money
+#     end
+#   end
+
 class Bank
   def initialize(money)
     @money = money
   end
 
-  def deposit(amount)
-    @money += amount
+  def withdraw(amount)
+    @money -= amount
+    return amount
   end
 
   def balance
-   p @money
+    @money
   end
 end
--- bank_spec.rb
+++ bank_spec.rb
@@ -1,20 +1,49 @@
 require "./bank"
 
 RSpec.describe Bank do
-  it "可以領錢" do
-    #Arrange
-    account = Bank.new(10)
-    
-    #Act
-    account.deposit(5)
+  Content "存錢功能" do
+    it "可以存錢" do
+      #Arrange
+      account = Bank.new(10)
+      
+      #Act
+      account.deposit(5)
 
-    #Assert
-    expect(account.balance).to be 15
+      #Assert
+      expect(account.balance).to be 15
+    end
 
-    it "不可以存0元或是小於0元的金額"
-    account = Bank.new(10)
-    expect { account.deposit(-5) }. to raise_exception BankError
+    #   it "不可以存0元或是小於0元的金額" do
+    #   account = Bank.new(10)
+    #   expect { account.deposit(-5) }. to raise_exception BankError
+    # end
+      it "不能領負的錢" do
+        account = Bank.new(10)
+        expect { account.withdraw(-3) }.to raise_exception BankError
+    end
   end
 end
 
+it "不能領超過餘額的錢" do
+  account = Bank.new(10)
+  expect { account.withdraw(20) }.to raise_exception BankError
+end
 
+class Bank
+
+    def initialize(money)
+      @money = money
+    end
+  
+    def withdraw(amount)
+      if 
+        amount <= 0
+        raise BankError.new("Amount must be greater than zero")
+        amount > money
+        raise BankError.new("You have not enough amount")
+      end
+  
+      def balance
+        @money
+      end
+    end

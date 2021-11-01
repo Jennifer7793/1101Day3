1b3d8d55c1e0c28fb499f28a32a9ca5e4c0625d1
LAPTOP-56NU4RUS\jenni <jennifer7793@gmail.com>
2021-11-01 14:38:22 +0800

    tdd

--- hi.rb
+++ hi.rb
@@ -1,5 +1,17 @@
 require 'sinatra'
 
-get "/" do
-  "hi 你好"
+# get "/" do
+#   "hi 你好"
+# end
+
+get "/about" do
+  "Success!"
+end
+
+# get "/aaaa/:xyz" do
+#   "hi #{params['xyz']}"
+# end
+
+get "/about" do
+  erb :about
 end
---
+++ tdd.rb
@@ -0,0 +1,9 @@
+def calc_score (x, y)
+  2 * x * y
+end
+
+if calc_score(3, 5) == 30
+  puts "success"
+else
+  puts "error"
+end
